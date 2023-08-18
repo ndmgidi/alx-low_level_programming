521bb1746845a876da413b5d161df246022b84b4
ndmgidi <nd.mgidi@gmail.com>
2023-08-18 09:29:34 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -17,7 +17,7 @@ void print_triangle(int size);
 void print_number(int n);
 int _putchar(char);
 int _isupper(int);
-int _isdigit(int);
+int _isdigit(int c);
 int mul(int, int);
 
 #endif /* main_h */
